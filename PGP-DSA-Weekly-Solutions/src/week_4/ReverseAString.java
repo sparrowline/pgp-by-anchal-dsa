60f54b6bef3a26b62dc0bfb7d8a9d6b8e6120e08
Syed Rizwan <syedrizwansat95@gmail.com>
2025-05-17 16:54:48 +0530

    Add solution of swap chars using Recursion;

--- PGP-DSA-Weekly-Solutions/src/week_4/ReverseAString.java
+++ PGP-DSA-Weekly-Solutions/src/week_4/ReverseAString.java
@@ -5,7 +5,7 @@ public class ReverseAString {
 	// tc of O(n) and sc of O(1) in two pointer approach instead of using stack;
 
 	public void reverseString(char[] s) {
-		// as mentioned in place hence cant use stack? will lead n space,
+		// as mentioned in place hence cant use stack. will lead n space,
 		// mostly we can use two pointer approach to reverse it two pointers by each
 		// othere.
 
@@ -19,16 +19,32 @@ public class ReverseAString {
 		// s[i]= st.pop();
 		// }
 
-		int left = 0;
-		int right = s.length - 1;
+//		int left = 0;
+//		int right = s.length - 1;
+//
+//		while (left < right) {
+//			char temp = s[left];
+//			s[left] = s[right];
+//			s[right] = temp;
+//			left++;
+//			right--;
 
-		while (left < right) {
-			char temp = s[left];
-			s[left] = s[right];
-			s[right] = temp;
-			left++;
-			right--;
-		}
+//		}
+		reverseStringHelper(s, 0, s.length - 1);
+	}
+
+	private void reverseStringHelper(char[] s, int left, int right) {
+		// base case condition;
+		if (left >= right)
+			return;
+
+		// swap the characters;
+		char temp = s[left];
+		s[left] = s[right];
+		s[right] = temp;
+
+		// recursive call;
+		reverseStringHelper(s, left + 1, right - 1);
 
 	}
 }

fdde16aa0a998ba5e0b6f3facd099172e3d73d0b
Syed Rizwan <syedrizwansat95@gmail.com>
2025-06-01 04:55:54 +0530

    add problem statement;

--- PGP-DSA-Weekly-Solutions/src/week_4/MinimumSumSubarray.java
+++ PGP-DSA-Weekly-Solutions/src/week_4/MinimumSumSubarray.java
@@ -1,7 +1,7 @@
 package week_4;
 
 import java.util.List;
-
+//https://leetcode.com/problems/minimum-positive-sum-subarray/description/?envType=problem-list-v2&envId=sliding-window
 public class MinimumSumSubarray {
 	    public int minimumSumSubarray(List<Integer> nums, int l, int r) {
 	        int result = Integer.MAX_VALUE;

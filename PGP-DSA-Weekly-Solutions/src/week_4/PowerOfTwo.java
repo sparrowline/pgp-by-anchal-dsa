fcb49feeeb169c8df4db1972822221eb1638b88f
Syed Rizwan <syedrizwansat95@gmail.com>
2025-06-03 16:16:14 +0530

    add recursion for powtow

--- PGP-DSA-Weekly-Solutions/src/week_4/PowerOfTwo.java
+++ PGP-DSA-Weekly-Solutions/src/week_4/PowerOfTwo.java
@@ -4,7 +4,7 @@ public class PowerOfTwo {
 	class Solution {
 		public boolean isPowerOfTwo(int n) {
 
-			// B.F;
+			// B.F:---------------------------------------
 			// while(n>0 && n%2==0){
 			// n = n/2;
 			// }
@@ -12,8 +12,16 @@ public class PowerOfTwo {
 			// return n==1;
 			// }
 
-			// optimized using bit manuplation;
-			return n > 0 && (n & (n - 1)) == 0;
+			//optimized using bit manipulation:-------------------------
+			//return n > 0 && (n & (n - 1)) == 0;
+			
+			
+			
+			// Using Recursion :--------------------
+	        if(n<=0) return false;
+	        if(n==1) return true;
+
+	        return ((n%2==0) && isPowerOfTwo(n/2));
 		}
 	}
 }

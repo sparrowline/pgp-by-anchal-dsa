467d1a9b338e602c2d84768c1b0df724f34b3a3a
Syed Rizwan <syedrizwansat95@gmail.com>
2025-06-17 16:47:13 +0530

    add powerSet for string type data.

--- PGP-DSA-Weekly-Solutions/src/week_4/PowerSet.java
+++ PGP-DSA-Weekly-Solutions/src/week_4/PowerSet.java
@@ -1,6 +1,7 @@
 package week_4;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 //https://www.youtube.com/watch?v=b7AYbpM5YrE&t=21s striver  explanation 
 public class PowerSet {
@@ -22,4 +23,26 @@ public class PowerSet {
 		}
 		return result;
 	}
+}
+
+//User function Template for Java for string type data.
+class AllPossibleStrings {
+ public List<String> AllPossibleStrings(String s) {
+     // Code here
+     List<String> result = new ArrayList<>();
+     int n = s.length();
+     int total = 1<<n;
+     
+     for(int i = 1;i<total;i++){
+         StringBuilder subset = new StringBuilder();
+         for(int j = 0;j<n;j++){
+             if((i & (1<<j))!=0){
+                 subset.append(s.charAt(j));
+             }
+         }
+         result.add(subset.toString());
+     }
+     Collections.sort(result);
+     return result;
+ }
 }

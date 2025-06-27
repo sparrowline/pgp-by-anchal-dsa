8c9c4501fa4d07eebeb0f11d57caba9316bffb96
Syed Rizwan <syedrizwansat95@gmail.com>
2025-06-27 12:17:02 +0530

    add predicate examples;

---
+++ PGP-DSA-Weekly-Solutions/src/java_8/PredicatePractice.java
@@ -0,0 +1,29 @@
+package java_8;
+
+import java.util.function.Predicate;
+
+//Predicate is a functional interface (boolean valued function)
+public class PredicatePractice {
+	
+	public static void main(String[] args) {
+		Predicate<Integer> isEven = x->(x%2)==0;
+		System.out.println("is Provided number is even : "+isEven.test(8));
+		
+		
+		//with string
+		Predicate<String> isWordStartsWithA = x->x.toLowerCase().startsWith("a");
+		System.out.println("is word start with A : "+isWordStartsWithA.test("Arafat"));
+		
+		Predicate<String> isWordEndsWithT = x->x.toLowerCase().endsWith("t");
+		System.out.println("is word end with t : "+ isWordEndsWithT.test("Arafat"));
+		
+		//combining two predicates
+		Predicate<String> combine = isWordStartsWithA.and(isWordEndsWithT);
+		System.out.println("is both conditions true? :" +combine.test("Arafat"));
+		
+		
+	
+	}
+	
+
+}
